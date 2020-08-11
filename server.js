b98a2ba8de74eec6bef0cad96e6cae02c6415aff
Darkrevenger-crypto <57855674+Darkrevenger-crypto@users.noreply.github.com>
2020-08-12 01:12:04 +0530

    'Deploy'

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //Set Port
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));

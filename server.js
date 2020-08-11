234cbcb274e177f9f5f4c37c52e07730ea092e2d
Darkrevenger-crypto <57855674+Darkrevenger-crypto@users.noreply.github.com>
2020-08-11 23:53:41 +0530

    Deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 const app = express();
 
 //connect db
@@ -11,6 +13,16 @@ app.use(express.json({ extended: false }));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/contests', require('./routes/chef'));
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 //Set Port
 const PORT = process.env.port || 5000;
